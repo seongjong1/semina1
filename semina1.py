0cecc1e8a8d7da45c4e41e87173ac0e1c0f552af
Kim SeongJong <bittjdwhd@gmail.com>
2024-10-04 15:42:47 +0900

    v0.0.5

--- semina1.py
+++ semina1.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-st.write("""
-    hello""")
-st.write("""hello2""")
+# st.write(""" hello""")
+# st.write("""hello2""")
+#
+# x = st.slider("X의 값 입력", 0,100,30) # 0~ 100의 범의의 slider , 초기값 50
+# y = np.sin(x)
+#
+# fig, ax = plt.subplots()
+# ax.plot([x],[y],marker="o")
+# st.pyplot(fig)
+#
+# st.write("과연 되나22222")
+# st.write("real ok")
+# st.write("JJIN MAK")
+# st.write("akakakakak")
 
-x = st.slider("X의 값 입력", 0,100,30) # 0~ 100의 범의의 slider , 초기값 50
-y = np.sin(x)
-
-fig, ax = plt.subplots()
-ax.plot([x],[y],marker="o")
-st.pyplot(fig)
-
-st.write("과연 되나22222")
-st.write("real ok")
-st.write("JJIN MAK")
-st.write("akakakakak")
+x = st.slider("X 값을 선택", 0, 100, 50)
+st.write(f"선택한 값: {x}")
