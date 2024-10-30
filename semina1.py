6118921b968c205e8b554ba15bc86cf563e518e5
main <bittjdwhd@naver.com>
2024-10-30 22:14:16 +0900

    update2

--- semina1.py
+++ semina1.py
@@ -3,26 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# st.write(""" hello""")
-# st.write("""hello2""")
-#
-# x = st.slider("X의 값 입력", 0,100,30) # 0~ 100의 범의의 slider , 초기값 50
-# y = np.sin(x)
-#
-# fig, ax = plt.subplots()
-# ax.plot([x],[y],marker="o")
-# st.pyplot(fig)
-#
-# st.write("과연 되나22222")
-# st.write("real ok")
-# st.write("JJIN MAK")
-# st.write("akakakakak")
+tab1,tab2 = st.tabs(["semina1", "semina2"])
 
-x = st.slider("X 값을 선택", 0, 100, 50)
-st.write(f"선택한 값: {x}")
+with tab1:
+    # 세션 상태에 semina_df가 없는 경우에만 데이터 로드 및 저장
+    x = st.slider("X 값을 선택", 0, 100, 50)
+    st.write(f"선택한 값: {x}")
 
-semina_df = pd.read_csv("joojong.csv")
-semina_df.set_index("연도",inplace=True)
-st.line_chart(semina_df)
-st.write("종백님이 AI인가, AI가 종백님인가?")
-st.write("되나")
+    semina_df = pd.read_csv("joojong.csv")
+    semina_df.set_index("연도", inplace=True)
+    st.line_chart(semina_df)
+
+with tab2:
+# 저장된 데이터 사용하기
+    if 'count' not in st.session_state:
+        st.session_state.count = 0  # 초기화
+
+    if st.button("카운트 증가"):
+        st.session_state.count += 1
+
+    st.write(f"현재 카운트: {st.session_state.count}")
+
+
